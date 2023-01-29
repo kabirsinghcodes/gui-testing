67562c9b4f96798ec02eba53653bb3166a1e6cd4
kabirsinghcodes <kabirsingh0405@gmail.com>
2023-01-29 22:25:16 +0530

    new file added

--- input.cpp
+++ input.cpp
@@ -3,21 +3,5 @@ using namespace std;
 
 int main(){
 
-  int array[100], temp[100];
-  int n, i, j, cnt;
-  cin >> n;
-  for (i = 0; i < n; i++){
-    cin >> array[i];
-    temp[i] = -1;
-  }
-
-
-  for (i = 0; i < n; i++){
-    if (temp[i] == 1){
-      cout << array[i] << "  ";
-    }
-  }
-
-  return 0;
 
 }
